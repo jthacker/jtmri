b0094ef35fdad1c6fa30ea4c24e67517d423759a
jthacker <jthacker@gmail.com>
2014-03-05 14:54:39 -0600

    Added subpackages to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
         name='jtmri',
         version='0.1dev',
-        packages=['jtmri'],
+        packages=['jtmri', 'jtmri.dcm',
+            'jtmri.scripts'],
         install_requires=[
             'numpy','traits','traitsui',
             'scikit-image','tqdm',
