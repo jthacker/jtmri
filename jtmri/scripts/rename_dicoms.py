1a259a7ff314d3dd63335f95e5f95e584a50dd0b
jthacker <jthacker@gmail.com>
2014-05-02 18:10:09 -0500

    rename_dicoms: added filtering, fixed output naming to include InstanceNumber, and print warnings when overwriting files

--- jtmri/scripts/rename_dicoms.py
+++ jtmri/scripts/rename_dicoms.py
@@ -1,39 +1,57 @@
+#!/usr/bin/env python
 import argparse
-import os.path
+import os
 import re
 import shutil
 import jtmri.dcm
+import sys
 
 _rgx = re.compile('[^\w\-\_]')
 
 def clean(attr):
     return _rgx.sub('_', attr)
 
+def filter_expr(expr_str):
+    if expr_str is None:
+        return lambda dcm:dcm
+    else:
+        return eval('lambda dcm: %s' % expr_str)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Rename a set of '
             'dicom files')
-    parser.add_argument('--outputdir', help='Directory to output renamed files to')
+    parser.add_argument('--ext', default='ima',
+            help='Set the extension to use for the renamed files')
+    parser.add_argument('--filter', default=None, type=filter_expr,
+            help='Filter the dicoms to rename, eg. dcm.SeriesNumber != 2. '
+                 'Expression can be any valid python code that works in a lambda expression')
     parser.add_argument('--move', action='store_true',
             help='Move files instead of copying')
+    parser.add_argument('--outputdir', help='Directory to output renamed files to',
+            metavar='DIR')
     parser.add_argument('--pretend', action='store_true',
             help='Show the renaming that will take place without actually performing it')
-    parser.add_argument('--extension', default='ima',
-            help='Set the extension to use for the renamed files')
     parser.add_argument('files', nargs='+')
     args = parser.parse_args()
+
+    if not args.pretend and args.outputdir and not os.path.exists(args.outputdir):
+        os.mkdir(args.outputdir)
 
     count = 0
     for f in args.files:
         dcms = jtmri.dcm.read(f, disp=False)
-        for dcm in dcms:
+        for dcm in dcms.filter(args.filter):
             attrs = [dcm.PatientName, dcm.Modality, dcm.StudyDescription, 
                     '%04d' % int(dcm.StudyID), '%04d' % dcm.SeriesNumber, 
-                    dcm.SeriesDate, dcm.AcquisitionTime, args.extension]
+                    '%04d' % dcm.InstanceNumber, dcm.SeriesDate, dcm.AcquisitionTime, 
+                    args.ext]
             filename = '.'.join(clean(attr) for attr in attrs)
             dirname = args.outputdir if args.outputdir else os.path.basename(dcm.filename)
             path = os.path.join(dirname, filename)
             print('%s -> %s' % (os.path.relpath(dcm.filename), os.path.relpath(path)))
             if not args.pretend:
+                if os.path.exists(path):
+                    print('Warning: Path already exists %s' % path)
                 if args.move:
                     shutil.move(dcm.filename, path)
                 else:
