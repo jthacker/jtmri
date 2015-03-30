24209646cfe85bae3f851276b7866e5bb5371cd2
Jon Thacker <thacker.jon@gmail.com>
2015-03-30 09:45:37 -0500

    Add ICC 3.1 and describe to stats

--- jtmri/stats.py
+++ jtmri/stats.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+import scipy.linalg
+import scipy.stats
 from collections import namedtuple
 import logging
+
 
 log = logging.getLogger('jtmri.stats')
 
@@ -75,3 +78,65 @@ def tests(data, groups, tests, stat, adj):
     df = df.set_index(['test'] + list(groups))
     df = df.sortlevel()
     return df
+
+
+def describe(arr):
+    funcs = [np.size, np.min, np.max, np.mean, np.median, np.var,
+             scipy.stats.skew, scipy.stats.kurtosis]
+
+    out = [(f.func_name, f(arr)) for f in funcs]
+    return pd.DataFrame(out, columns=['statistic', 'value']).set_index('statistic')
+
+
+def icc_3_1(Y):
+    '''
+    Calculates Interclass Correlation Coefficient (3,1) as defined in
+    P. E. Shrout & Joseph L. Fleiss (1979). "Intraclass Correlations: Uses
+    in Assessing Rater Reliability".
+    This particular implementation is aimed at relaibility (test-retest) studies.
+    @see https://github.com/nipy/nipype/blob/44f75665bf4776d7e6ff63395254de3111da8ce5/nipype/algorithms/icc.py#L23
+    '''
+
+    [nb_subjects, nb_conditions] = Y.shape
+    dfc = nb_conditions - 1
+    dfe = (nb_subjects - 1) * dfc
+    dfr = nb_subjects - 1
+
+    # Compute the repeated measure effect
+    # ------------------------------------
+
+    # Sum Square Total
+    mean_Y = np.mean(Y)
+    SST = ((Y - mean_Y) ** 2).sum()
+
+    # create the design matrix for the different levels
+    x = np.kron(np.eye(nb_conditions), np.ones((nb_subjects, 1)))  # sessions
+    x0 = np.tile(np.eye(nb_subjects), (nb_conditions, 1))  # subjects
+    X = np.hstack([x, x0])
+
+    # Sum Square Error
+    predicted_Y = np.dot(np.dot(np.dot(X, scipy.linalg.pinv(np.dot(X.T, X))), X.T), Y.flatten('F'))
+    residuals = Y.flatten('F') - predicted_Y
+    SSE = (residuals ** 2).sum()
+
+    residuals.shape = Y.shape
+
+    MSE = SSE / dfe
+
+    # Sum square session effect - between colums/sessions
+    SSC = ((np.mean(Y, 0) - mean_Y) ** 2).sum() * nb_subjects
+    MSC = SSC / dfc / nb_subjects
+
+    session_effect_F = MSC / MSE
+
+    # Sum Square subject effect - between rows/subjects
+    SSR = SST - SSC - SSE
+    MSR = SSR / dfr
+
+    # ICC(3,1) = (mean square subjeT - mean square error) / (mean square subjeT + (k-1)*-mean square error)
+    ICC = (MSR - MSE) / (MSR + dfc * MSE)
+
+    e_var = MSE #variance of error
+    r_var = (MSR - MSE)/nb_conditions #variance between subjects
+
+    return ICC, r_var, e_var, session_effect_F, dfc, dfe
